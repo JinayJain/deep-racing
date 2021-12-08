a57861f5a7b1075d68f614034e077f404989a2f3
Jinay Jain <jinaybjain@gmail.com>
2021-12-08 11:01:21 -0500

    add basic episode plotting

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from math import e
 import gym
 import numpy as np
 import torch
@@ -6,6 +5,7 @@ from torch import nn, optim
 from torchvision.models import efficientnet_b0
 import torchvision.transforms as T
 import yaml
+import visdom
 
 from itertools import count
 
@@ -16,6 +16,7 @@ from memory import ReplayBuffer
 CONFIG_FILE = "config.yml"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+vis = visdom.Visdom()
 
 
 def load_config():
@@ -83,20 +84,12 @@ def main():
         if len(memory) < cfg["batch_size"]:
             return
 
-        critic_opt.zero_grad()
-
         batch = memory.sample(cfg["batch_size"])
         states, actions, rewards, next_states = zip(*batch)
 
         states = torch.cat(states, dim=0).to(device)
         actions = torch.cat(actions, dim=0).to(device)
         rewards = torch.tensor(rewards).to(device).unsqueeze(1)
-
-        # Compute predicted Q-values
-        q_pred = critic(states, actions)
-
-        # Compute target Q-values
-        q_target = rewards
 
         # Handle terminal states
         non_final_mask = torch.tensor(
@@ -107,6 +100,13 @@ def main():
 
         next_states = torch.cat([s for s in next_states if s is not None]).to(device)
 
+        critic_opt.zero_grad()
+
+        # Compute predicted Q-values
+        q_pred = critic(states, actions)
+
+        # Compute target Q-values
+        q_target = rewards
         q_target[non_final_mask] += cfg["gamma"] * target_critic(
             next_states, target_actor(next_states)
         )
@@ -122,21 +122,41 @@ def main():
         actor_loss.backward()
         actor_opt.step()
 
+    episode_rewards = []
+
+    # Create episode reward plot
+    win_ep_reward = vis.line(
+        X=np.array([0]),
+        Y=np.array([0]),
+        opts=dict(title="Episode Rewards", xlabel="Episode", ylabel="Reward",),
+    )
+
     for ep in range(cfg["num_episodes"]):
         with torch.no_grad():
             screen = env.reset()
             state = effnet(preprocess(screen).unsqueeze(0).to(device))
 
+        last_reward_step = 0
+        total_reward = 0
         for t in count():
             # Create a new transition to store in the replay buffer
             with torch.no_grad():
                 # Sample an action from the policy (noise is added to ensure exploration)
-                action = actor(state).cpu()
-                action += torch.randn(action.shape) * cfg["noise_std"]
+                action = actor(state)
+                action += torch.randn(action.shape).to(device) * cfg["noise_std"]
+
+                # Ask the critic for the Q-value estimate of the current state and action
+                q_value = critic(state, action)
+
+                action = action.cpu()
 
                 screen, reward, done, _ = env.step(action.numpy()[0])
+                total_reward += reward
 
-                if done:
+                if reward > 0:
+                    last_reward_step = t
+
+                if done or (t - last_reward_step) > cfg["max_steps_without_reward"]:
                     memory.push(state.cpu(), action, reward, None)
                     break
 
@@ -150,9 +170,27 @@ def main():
             soft_update(target_actor, actor, cfg["tau"])
             soft_update(target_critic, critic, cfg["tau"])
 
-            env.render()
+            if cfg["render"]:
+                env.render()
 
             state = next_state
+
+        print(f"Episode {ep} finished after {t} timesteps")
+
+        episode_rewards.append(total_reward)
+
+        # Update the plot
+        vis.line(
+            X=np.array([ep]),
+            Y=np.array([total_reward]),
+            win=win_ep_reward,
+            name="Episode Reward",
+            update="append",
+        )
+
+        if ep % cfg["save_every"] == 0:
+            torch.save(actor.state_dict(), f"ckpt/actor_{ep}.pt")
+            torch.save(critic.state_dict(), f"ckpt/critic_{ep}.pt")
 
     env.close()
 
